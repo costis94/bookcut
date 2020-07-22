4f6307fad3f7bc2c61754d6914aad57fbc567ac5
costis94 <gravitymusician@gmail.com>
2020-07-22 11:12:34 +0300

    Installs only with Python3!

--- setup.py
+++ setup.py
@@ -1,7 +1,15 @@
 import setuptools
+import sys
+
+if sys.version_info.major < 3:
+    print("\nPython 2 is not supported! \nPlease upgrade to Python 3.\n")
+    print("Installation of BookCut stopped, please try again with\na newer version of Python!")
+    sys.exit(1)
+
 
 setuptools.setup(
-    name="BookCat", # Replace with your own username
+    name="BookCat",
+    python_requires='>3.5.2',
     version="0.5",
     author="Costis94",
     author_email="gravitymusician@gmail.com",

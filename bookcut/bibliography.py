aeb557a9b6b59c3461ddf22401cf38327fbeb67d
costis94 <gravitymusician@gmail.com>
2021-01-15 14:46:08 -0600

    Added openLibraryStatus() which checks if OpenLib is online.

--- bookcut/bibliography.py
+++ bookcut/bibliography.py
@@ -3,7 +3,7 @@ import json
 import re
 from difflib import SequenceMatcher
 import os
-
+from bookcut.mirror_checker import openLibraryStatus
 
 def similar(a, b, similarity):
     ''' function which check similarity between two strings '''
@@ -17,44 +17,45 @@ def similar(a, b, similarity):
 def main(author, similarity):
     # returns all the books writen by an author from openlibrary
     # using similarity for filtering the results
+    status = openLibraryStatus()
+    if status is not False:
+        search_url = "http://openlibrary.org/search.json?author=" + author
+        jason = requests.get(search_url)
+        jason = jason.text
+        data = json.loads(jason)
+        data = data['docs']
+        if data != []:
+            metr = 0
+            books = []
+            for i in range(0, len(data)-1):
+                title = data[metr]['title']
+                metr = metr + 1
+                books.append(title)
+                mylist = list(dict.fromkeys(books))
 
-    search_url = "http://openlibrary.org/search.json?author=" + author
-    jason = requests.get(search_url)
-    jason = jason.text
-    data = json.loads(jason)
-    data = data['docs']
-    if data != []:
-        metr = 0
-        books = []
-        for i in range(0, len(data)-1):
-            title = data[metr]['title']
-            metr = metr + 1
-            books.append(title)
-            mylist = list(dict.fromkeys(books))
+        #       Filtrering results: trying to erase similar titles
+            words = [' the ', 'The ', ' THE ', ' The' ' a ', ' A ', ' and ', ' of ',
+                     ' from ', 'on', 'The', 'in']
 
-    #       Filtrering results: trying to erase similar titles
-        words = [' the ', 'The ', ' THE ', ' The' ' a ', ' A ', ' and ', ' of ',
-                 ' from ', 'on', 'The', 'in']
-
-        noise_re = re.compile('\\b(%s)\\W'%('|'.join(map(re.escape,words))),re.I)
-        clean_mylist = [noise_re.sub('', p) for p in mylist]
+            noise_re = re.compile('\\b(%s)\\W'%('|'.join(map(re.escape,words))),re.I)
+            clean_mylist = [noise_re.sub('', p) for p in mylist]
 
 
-        for i in clean_mylist:
-            for j in clean_mylist:
-                a = similar(i, j, similarity)
-                if a == True:
-                    clean_mylist.pop(a)
+            for i in clean_mylist:
+                for j in clean_mylist:
+                    a = similar(i, j, similarity)
+                    if a == True:
+                        clean_mylist.pop(a)
 
-        clean_mylist.sort()
-        print(' ~Books found to OpenLibrary Database:\n')
-        for i in clean_mylist:
-            print(i)
-        return clean_mylist
-    else:
-        print('(!) No valid author name, or bad internet connection.')
-        print('Please try again!')
-        return None
+            clean_mylist.sort()
+            print(' ~Books found to OpenLibrary Database:\n')
+            for i in clean_mylist:
+                print(i)
+            return clean_mylist
+        else:
+            print('(!) No valid author name, or bad internet connection.')
+            print('Please try again!')
+            return None
 
 
 def save_to_txt(lista, path, author):

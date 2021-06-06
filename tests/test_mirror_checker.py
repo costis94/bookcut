5c31da5ed764087dc1d00bd18cf5ea18a9a0a168
Paul Reiter <reiter.paul@gmail.com>
2021-06-06 18:24:53 +0200

    Remove now unneccessary global statement

--- tests/test_mirror_checker.py
+++ tests/test_mirror_checker.py
@@ -4,7 +4,6 @@ from bookcut.mirror_checker import openLibraryStatus, main as mirror_checker
 
 @pytest.mark.web
 def test_mirror_availability():
-    global available_mirror
     available_mirror = mirror_checker()
     assert type(available_mirror) is str, "Not correct type of LibGen Url"
     assert available_mirror.startswith('http'), "Not correct LibGen Url."
